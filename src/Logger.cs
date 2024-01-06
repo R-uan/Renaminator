de020a45d77aa74ee47a8f4600861cdd680129f8
Ruan <ruan.oliveiralopes@hotmail.com>
2024-01-06 09:17:21 -0300

    Create Logger.cs
    - move logger to it's own file

---
+++ src/Logger.cs
@@ -0,0 +1,18 @@
+﻿namespace Renaminator;
+
+public class Logger {
+	public static void Log(string OldName, string NewName) {
+		string Now = DateTime.Now.ToShortTimeString();
+		Console.WriteLine($"[{LogLevel.INFO} * {Now}] : '{OldName}' renamed to '{NewName}'.");
+	}
+
+	public static void Log(string OldName, string Message, LogLevel Level) {
+		string Now = DateTime.Now.ToShortTimeString();
+		if (Level == LogLevel.INFO) {
+			Console.WriteLine($"[{Level} * {Now}] : '{OldName}': {Message}.");
+		} else if (Level == LogLevel.ERROR) {
+			Console.WriteLine($"[{Level} * {Now}] : Failed to rename '{OldName}': {Message}.");
+		}
+	}
+
+}
